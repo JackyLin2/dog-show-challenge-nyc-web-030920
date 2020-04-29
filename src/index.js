73114ef65f444e004daa37a335ed7b8f5cc3ea1f
JackyLin2 <jacky.jia.lin@gmail.com>
2020-04-29 11:41:28 -0400

    closed

--- src/index.js
+++ src/index.js
@@ -23,15 +23,18 @@ document.addEventListener('DOMContentLoaded', () => {
            dogForm.breed.value = `${dog.breed}`
            dogForm.sex.value = `${dog.sex}`
             })
-        let submitDog = document.getElementById("dog-form")
+        
+            let submitDog = document.getElementById("dog-form")
         submitDog.addEventListener("submit",function(e){
             e.preventDefault()
+            console.log(dog)
             const form = event.target 
             const name = form.name.value 
             const breed = form.breed.value
             const sex = form.sex.value 
 
             const newDog = {name, breed, sex}
+           
         })
 
         function insertDog(newDog){
@@ -40,7 +43,23 @@ document.addEventListener('DOMContentLoaded', () => {
                 headers: requestHeaders,
                 body: JSON.stringify(newDog)
             })
-            .then(resp
+            .then((resp) => resp.json())
+            .then((dog) => {
+                addDog(dog)
+            })
+        }
+
+        function addDog(dog){
+            let perDog = document.createElement("tr")
+           let id = perDog.dataset.id = dog.id 
+           perDog.innerHTML = `
+           <tr><td>${dog.name}</td> 
+           <td>${dog.breed}</td> 
+           <td>${dog.sex}</td>
+            <td><button id= ${dog.id}>Edit</button></td>
+            </tr>
+           `
+           registeredDog.appendChild(perDog)
         }
        })
     })
