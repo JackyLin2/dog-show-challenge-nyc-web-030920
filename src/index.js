e6c1fb9498da6eb699b04f6f3752a7ec415565dd
JackyLin2 <jacky.jia.lin@gmail.com>
2020-04-29 11:12:00 -0400

    one hour

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
+    requestHeaders = {"accept": "application/json",
+    "content-type": "application/json"}
+    const registeredDog = document.getElementById("table-body")
+    fetch("http://localhost:3000/dogs")
+    .then((resp) => resp.json())
+    .then((data) => {
+       data.forEach(function(dog){
+           let perDog = document.createElement("tr")
+           let id = perDog.dataset.id = dog.id 
+           perDog.innerHTML = `
+           <tr><td>${dog.name}</td> 
+           <td>${dog.breed}</td> 
+           <td>${dog.sex}</td>
+            <td><button id= ${dog.id}>Edit</button></td>
+            </tr>
+           `
+           registeredDog.appendChild(perDog)
+         const dogEdit = document.getElementById(`${dog.id}`)
+         dogEdit.addEventListener('click', function(event){
+            const dogForm = document.getElementById("dog-form")
+           dogForm.name.value = `${dog.name}`
+           dogForm.breed.value = `${dog.breed}`
+           dogForm.sex.value = `${dog.sex}`
+            })
+        let submitDog = document.getElementById("dog-form")
+        submitDog.addEventListener("submit",function(e){
+            e.preventDefault()
+            const form = event.target 
+            const name = form.name.value 
+            const breed = form.breed.value
+            const sex = form.sex.value 
 
+            const newDog = {name, breed, sex}
+        })
+
+        function insertDog(newDog){
+            fetch(`http://localhost:3000/dogs/${id}`, {
+                method: "PATCH",
+                headers: requestHeaders,
+                body: JSON.stringify(newDog)
+            })
+            .then(resp
+        }
+       })
+    })
 })
